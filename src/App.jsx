fd8d3a314676ce7917cb183baf83167d64ba061a
CodedByYoyo <codedbyyoyo@gmail.com>
2025-09-04 11:26:21 +0400

    made first part of hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import gsap from "gsap"
 import { ScrollTrigger, SplitText } from "gsap/all"
 import Navbar from "./components/Navbar"
+import Hero from "./components/hero"
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
@@ -10,6 +11,10 @@ function App() {
   return (
     <main>
       <Navbar />
+      <Hero />
+      <div className="h-dvh bg-black">
+
+      </div>
     </main>
   )
 }
